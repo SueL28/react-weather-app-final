11fb44e26680624fb4274cd918ec60266ebbcbee
SueL28 <69327405+SueL28@users.noreply.github.com>
2021-08-31 22:07:58 -0400

    removed imports that were not used in date and time format component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+
 
 export default function FormattedDate(props){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -7,7 +7,7 @@ export default function FormattedDate(props){
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let month = months[props.date.getMonth()];
     let date = props.date.getDate();
-    
+
     return `${day} ${month} ${date}`
   
 }
